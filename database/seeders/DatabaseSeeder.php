a1e3bd64f4b27464b878ba75a70e4b04ed088133
Glenn Raya <rayaglenn@gmail.com>
2024-06-03 15:17:34 +0800

    Change seeding logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        $roles = [
+            'Software Engineer', 'DevOps', 'Database Analyst', 'Cyber Security Consultant', 'Project Manager',
+            'System Architect', 'QA Engineer', 'Product Owner', 'UI/UX Designer', 'Scrum Master'
+        ];
+        shuffle($roles);
+
+        foreach ($roles as $role) {
+            User::create([
+                'name' => fake()->name(),
+                'role' => $role,
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+        // User::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
